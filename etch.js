142ab20491aee2c7cd45af26867d737b9a82dcc1
KayaSake <mikehferguson@gmail.com>
2023-03-28 12:51:50 -0600

    Commit

---
+++ etch.js
@@ -0,0 +1,10 @@
+const container = document.querySelector("#container")
+
+const content = document.getElementById('div');
+content.classList.add('content')
+container.appendChild(content)
+
+const grid = content.createElement('div')
+grid.classList.add('grid')
+container.appendChild(grid)
+
